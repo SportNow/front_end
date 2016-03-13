673e2ac35db7198f34bc54bfa6cebb250944aad3
neokevinwu <wukevinw@sfu.ca>
2016-03-13 19:55:19 +0000

    sport-now-login and sport-now-sport-select

--- nima_html/js/scripts.js
+++ nima_html/js/scripts.js
@@ -12,4 +12,25 @@ $( document ).ready( function() {
   });
   
   
+/*  $(function() {
+    sliders =  ["amount1", "amount2", "amount3", "amount4", "amount5", "amount6", "amount7", "amount8", "amount9"];
+    
+    $( ".sport-slider" ).each(function(index) {
+      sslider = "#slider"+ (index+1)
+        $(sslider).slider({
+        value:5,
+        min: 0,
+        max: 10,
+        step: 1,
+        slide: function( event, ui ) {
+          $( "#"+sliders[index] ).val(  ui.value );
+        }
+      });
+      
+       $( "#"+sliders[index] ).val( $( sslider ).slider( "value" ) );
+    })
+   
+  });
+  */
+  
 });
