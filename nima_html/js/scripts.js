463709519689c1973a00a795e70bac311ae26ec1
neokevinwu <wukevinw@sfu.ca>
2016-03-13 22:47:26 +0000

    sport-now-sport-select 90% complete

--- nima_html/js/scripts.js
+++ nima_html/js/scripts.js
@@ -5,14 +5,18 @@ $( document ).ready( function() {
   });
 
   // With JQuery
-  $('#ex1').slider({
-    formatter: function(value) {
-      return value;
-    }
-  });
+  $("#bootstrap-slider1").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider2").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider3").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider4").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider5").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider6").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider7").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider8").slider({ min: 0, max: 10, value: 5, focus: true });
+  $("#bootstrap-slider9").slider({ min: 0, max: 10, value: 5, focus: true });
   
   
-  $(function() {
+/*  $(function() {
     sliders =  ["amount1", "amount2", "amount3", "amount4", "amount5", "amount6", "amount7", "amount8", "amount9"];
     
     $( ".sport-slider" ).each(function(index) {
@@ -30,7 +34,7 @@ $( document ).ready( function() {
        $( "#"+sliders[index] ).val( $( sslider ).slider( "value" ) );
     })
    
-  });
+  });*/
   
   
 });
