d75bcb490e57e8e928b6d1dbbc6236e35cbe309d
neokevinwu <wukevinw@sfu.ca>
2016-03-27 21:26:06 +0000

    sport event in progress

--- nima_html/js/scripts.js
+++ nima_html/js/scripts.js
@@ -29,25 +29,6 @@ $( document ).ready( function() {
   $("#bootstrap-slider9").slider({ min: 0, max: 10, value: 5, focus: true });
   
   
-/*  $(function() {
-    sliders =  ["amount1", "amount2", "amount3", "amount4", "amount5", "amount6", "amount7", "amount8", "amount9"];
-    
-    $( ".sport-slider" ).each(function(index) {
-      sslider = "#slider"+ (index+1)
-        $(sslider).slider({
-        value:5,
-        min: 0,
-        max: 10,
-        step: 1,
-        slide: function( event, ui ) {
-          $( "#"+sliders[index] ).val(  ui.value );
-        }
-      });
-      
-       $( "#"+sliders[index] ).val( $( sslider ).slider( "value" ) );
-    })
-   
-  });*/
-
+  $("#ex2").slider({});
   
 });
